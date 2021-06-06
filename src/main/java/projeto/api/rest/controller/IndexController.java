c0da717ef960e25f8ad68b6857c20c75e48ebaf4
mlima3696 <mjuniorlima3@gmail.com>
2021-06-06 17:40:25 -0300

    Criando a remoção com DELETE

--- src/main/java/projeto/api/rest/controller/IndexController.java
+++ src/main/java/projeto/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -102,4 +103,21 @@ public class IndexController {
 		return new ResponseEntity("Venda atualizada",HttpStatus.OK);
 	}
 	
+	@DeleteMapping(value="/{id}",produces="application/text")
+	public String delete (@PathVariable("id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+		
+	}
+	
+	@DeleteMapping(value="/{id}/venda",produces="application/text")
+	public String deletevenda (@PathVariable("id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "ok";
+		
+	}
 }
