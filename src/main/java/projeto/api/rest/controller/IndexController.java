0da12d3ffe055e0543b3c45ef3ff4f202efcdde3
mlima3696 <mjuniorlima3@gmail.com>
2021-06-06 12:01:13 -0300

    Criando a atualização de cadastro com o PUT

--- src/main/java/projeto/api/rest/controller/IndexController.java
+++ src/main/java/projeto/api/rest/controller/IndexController.java
@@ -1,6 +1,5 @@
 package projeto.api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,7 +80,26 @@ public class IndexController {
 		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity("id user: " + iduser + " idvenda: " +idvenda,HttpStatus.OK);
-		
 	}	
+	
+	@PutMapping(value = "/", produces="application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario) {
+		
+		//Outras rotinas antes de atualizar
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo,HttpStatus.OK);
+		
+	}
+	
+	@PutMapping(value = "/{iduser}/idvenda/{idvenda}", produces="application/json")
+	public ResponseEntity updatevenda(@PathVariable Long iduser,
+			                                       @PathVariable Long idvenda) {
+		
+		//Aqui seria o processo de venda
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("Venda atualizada",HttpStatus.OK);
+	}
 	
 }
