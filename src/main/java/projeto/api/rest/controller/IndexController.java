18293aabfc6cf3103fd4e778045b4aa4df318cc7
mlima3696 <mjuniorlima3@gmail.com>
2021-06-06 00:03:40 -0300

    Criando o serviço POST e efetuando um cadastro de usuário

--- src/main/java/projeto/api/rest/controller/IndexController.java
+++ src/main/java/projeto/api/rest/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,5 +52,35 @@ public class IndexController {
 		return new ResponseEntity<List<Usuario>> (list, HttpStatus.OK);
 	}
 	
+	
+	@PostMapping(value = "/", produces="application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario) {
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo,HttpStatus.OK);
+		
+	}
+		
+	/*@PostMapping(value = "/vendausuario", produces="application/json")
+	public ResponseEntity<Usuario> cadastrarvenda(@RequestBody Usuario usuario) {
+		
+		//Aqui seria o processo de venda
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo,HttpStatus.OK);
+		
+	}*/
+	
+	@PostMapping(value = "/{iduser}/idvenda/{idvenda}", produces="application/json")
+	public ResponseEntity cadastrarvenda1(@PathVariable Long iduser,
+			                                       @PathVariable Long idvenda) {
+		
+		//Aqui seria o processo de venda
+		//Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity("id user: " + iduser + " idvenda: " +idvenda,HttpStatus.OK);
+		
+	}	
 	
 }
